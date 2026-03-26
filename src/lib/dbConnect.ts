5e7f7a2bdd367c03a9942259bf71a9dabc528876
Ameer Hamza <ameerhamzainam@gmail.com>
2026-03-26 19:07:54 +0500

    DB connection setup

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+interface DBConnectOptions {
+  isConnected?: number;
+}
+
+const connection: DBConnectOptions = {
+  isConnected: 0,
+};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to the database.");
+    return;
+  }
+  try{
+    const db = await mongoose.connect(process.env.MONGODB_URI || '',{});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Connected to the database.");
+  }
+  catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+}
+export default dbConnect;
